f418b94c84520d4f5dbc9f98743a44eedeca70e6
anitah-nitty <anitahreh856@gmail.com>
2026-02-03 13:19:05 +0300

    initial commit

---
+++ variables.py
@@ -0,0 +1,23 @@
+#variable is a container used to store a value
+
+School = "Emobilis"
+Firstname = "Anitah"
+Lastname = "Wacuka"
+Num1 = 45
+Num2 = 100
+Language = "python"
+
+
+print(School)
+print(Num1 + Num2)
+print(Firstname,Lastname)
+print(School,"is a coding school")
+print(Language,"is a web programing language")
+
+
+
+number = 45
+numb = 100
+numberr = 145
+
+print("The sum of",Num1,"and", Num2, "is" ,Num1 * Num2)
